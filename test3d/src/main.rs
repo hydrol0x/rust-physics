6136c2fa0fa3c4c08f23d283110d103fe270596d
hydrol0x <mrjacobry@gmail.com>
2023-08-17 22:34:41 -0400

    dampening force, fixed bug where i actually add momentum now

--- test3d/src/main.rs
+++ test3d/src/main.rs
@@ -16,27 +16,28 @@ struct Particle {
     pos: Vector3<f32>,
     momentum: Vector3<f32>,
     mass: f32,
+    radius: f32,
 }
 
 fn main() {
     let mut window = Window::new("Kiss3d: cube");
     window.set_light(Light::StickToCamera);
-    const NUM_PARTICLES: usize = 5;
+    const NUM_PARTICLES: usize = 10;
     let (mut window, mut particles) = create_particles(NUM_PARTICLES, window);
 
     let mut rng = rand::thread_rng();
     for particle in &mut particles {
-        let x: f32 = rng.gen_range(0.0..=10.0); // generates a float between 0 and 10
-        let y: f32 = rng.gen_range(0.0..=10.0); // generates a float between 0 and 10
-        let z: f32 = rng.gen_range(0.0..=10.0); // generates a float between
+        let x: f32 = rng.gen_range(0.0..=100.0); // generates a float between 0 and 10
+        let y: f32 = rng.gen_range(0.0..=100.0); // generates a float between 0 and 10
+        let z: f32 = rng.gen_range(0.0..=100.0); // generates a float between
         let px: f32 = rng.gen_range(5.0..=10.0); // generates a float between 0 and 10
         let py: f32 = rng.gen_range(5.0..=10.0); // generates a float between 0 and 10
         let pz: f32 = rng.gen_range(5.0..=10.0); // generates a float between
-        translate(particle, x, y, z);
-        particle.momentum = Vector3::new(px, py, pz);
+        translate(particle, x, y, z); // adds translation
+        particle.momentum = Vector3::new(0.0 * px, 0.0 * py, 0.0 * pz);
     }
 
-    const dt: f32 = 0.00000001;
+    const dt: f32 = 0.01;
     // const dt: f32 = 1.0;
     const iters_per_sec: f32 = 60.0;
     let time_interval = Duration::from_secs_f32(1.0 / iters_per_sec);
@@ -72,21 +73,11 @@ fn main() {
             for i in 0..dps.len() {
                 let particle = &mut particles[i];
                 let imp = dps[i];
-                let dr = imp / particle.mass;
-                println!("{}", dr);
+                particle.momentum += imp;
+                let dr = (particle.momentum / particle.mass) * dt;
+                // println!("{}", dr);
                 translate(particle, dr.x, dr.y, dr.z);
             }
-
-            // for particle in &mut particles {
-            //     let dx = (particle.momentum.x / particle.mass) * dt;
-            //     let dy = (particle.momentum.y / particle.mass) * dt;
-            //     let dz = (particle.momentum.z / particle.mass) * dt;
-            //     translate(particle, dx, dy, dz);
-            //     // println!("{}", particle.pos.magnitude());
-            //     if (particle.pos.magnitude() >= 0.2) {
-            //         particle.momentum = -particle.momentum; // figure out how to bound them
-            //     }
-            // }
 
             accumulator -= time_interval;
         }
@@ -98,9 +89,15 @@ fn main() {
 }
 
 fn translate(particle: &mut Particle, x: f32, y: f32, z: f32) {
-    particle.pos = Vector3::new(x, y, z);
+    // TODO: change name of function to add_translation or something and or
+    //  Make this a function that just translates to a position x,y,z
+    // and a separet functin add_translation that wraps around
+    // particle.pos = Vector3::new(x, y, z);
+    particle.pos.x += x;
+    particle.pos.y += y;
+    particle.pos.z += z;
     let t = Translation3::new(x, y, z);
-    particle.sphere.prepend_to_local_translation(&t);
+    particle.sphere.append_translation(&t);
 }
 
 fn particle(sphere: SceneNode, pos: (f32, f32, f32), mom: (f32, f32, f32), mass: f32) -> Particle {
@@ -111,6 +108,7 @@ fn particle(sphere: SceneNode, pos: (f32, f32, f32), mom: (f32, f32, f32), mass:
         pos: position,
         momentum: momentum,
         mass: mass,
+        radius: 1.0,
     }
 }
 
@@ -127,19 +125,15 @@ fn create_particles(num_p: usize, mut window: Window) -> (Window, Vec<Particle>)
     }
     (window, particles)
 }
-
 fn grav_force(particle1: &Particle, particle2: &Particle) -> Vector3<f32> {
     // Gmm/r^2
-    const G: f32 = 1e-11;
+    const G: f32 = 1000.0;
     let r = particle2.pos - particle1.pos;
-    // print!("distance {}", r.norm());
-    let rhat = r / r.norm(); // Orient the force vector
-    let force = (G * particle1.mass * particle2.mass) / f32::powf(r.magnitude(), 2.0);
-    rhat * force
-}
+    let distance = r.norm();
 
-fn apply_impulse(particle: &mut Particle, force: Vector3<f32>, dt: f32) {
-    let imp = force * dt;
-    particle.momentum += imp;
-    // particle.momentum
+    let rhat = r / r.norm(); // Orient the force vector
+    let force = (G * particle1.mass * particle2.mass) / f32::powf(r.norm(), 2.0);
+    const K: f32 = 0.5;
+    let dampen_force = (1.0 / (1.0 + K * distance)) * force;
+    rhat * (force - dampen_force)
 }
