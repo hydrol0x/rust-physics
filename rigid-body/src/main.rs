48f263990cf240ed23c64f47dc6cf2cbe85b1127
hydrol0x <mrjacobry@gmail.com>
2023-08-20 19:34:58 -0400

    created rigid body init commit

---
+++ rigid-body/src/main.rs
@@ -0,0 +1,7 @@
+struct Spring {
+    stiffness: f32, // Spring constant
+    length: f32,
+    pos_1: f32, // first end of spring position
+    pos_2: f32, // second end of spring position
+}
+fn main() {}
